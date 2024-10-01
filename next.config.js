8e5a501292f0521f6e8362fe7fad67d0050506dd
Logan-Myn <logan.moyon15@gmail.com>
2024-10-01 10:33:23 +0300

    next js config

--- next.config.js
+++ next.config.js
@@ -14,4 +14,10 @@ const nextConfig = {
   output: 'standalone',
 };
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    config.experiments = { asyncWebAssembly: true, layers: true };
+    config.optimization.nodeEnv = false;
+    return config;
+  },
+}
